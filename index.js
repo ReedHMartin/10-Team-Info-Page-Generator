27c02a032bc819d2a8459587881f40f16c487b7b
Reed Martin <reedhmartin@gmail.com>
2023-04-16 05:49:33 -0400

    Added dist folder and css file. Added function to start the app with several TODOs and function to write to team.html.

--- index.js
+++ index.js
@@ -8,3 +8,34 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const renderEmployeeCard = require('./src/renderCards');
+
+// Placeholder array to store team members
+const teamMembers = [];
+
+// Main function to start the application
+async function main() {
+  // Prompt user for manager information
+  // TODO: Add inquirer prompts for manager details
+
+  // Create a new Manager object and add it to the teamMembers array
+  const manager = new Manager(/* manager details */);
+  teamMembers.push(manager);
+
+  // Prompt user to add engineers, interns or finish building the team
+  // TODO: Add inquirer prompts to choose the next action
+
+  // Loop through adding engineers/interns until the user chooses to finish building the team
+  // TODO: Add logic to handle user choices and add team members accordingly
+
+  // Generate HTML cards for each team member
+  const teamCardsHTML = teamMembers.map(renderEmployeeCard).join('');
+
+  // Generate the final HTML file with teamCardsHTML
+  // TODO: Add a function to generate the final HTML file
+
+  // Write the HTML file to the 'dist' directory
+  fs.writeFileSync(path.join(__dirname, 'dist', 'team.html'), finalHTML);
+}
+
+// Run the main function
+main();
