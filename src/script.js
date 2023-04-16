e4eacc73de44a396c836fd9c69f297a07d14fada
Reed Martin <reedhmartin@gmail.com>
2023-04-16 06:43:38 -0400

    Import inquirer, node modules, and classses from lib.

--- src/script.js
+++ src/script.js
@@ -1,2 +1,13 @@
-// Import Inquirer package
+// Import Inquirer 
 const inquirer = require('inquirer');
+
+// Import modules from Node.js
+const fs = require('fs');
+const path = require('path');
+
+// Import classes from 'lib' folder
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
