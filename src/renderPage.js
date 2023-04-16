5259bb7ff1c6f34d03d235e6eaec9facb718276b
Reed Martin <reedhmartin@gmail.com>
2023-04-16 12:20:23 -0400

    Added and completed render  page file.

---
+++ src/renderPage.js
@@ -0,0 +1,25 @@
+// Function to render the entire HTML page with team members' information
+function renderPage(teamMembers) {
+    // HTML code for the entire page
+    const pageHTML = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>My Team</title>
+        </head>
+        <body>
+          <header>
+            <h1>My Team</h1>
+          </header>
+          <main>
+            ${teamMembers.map(member => renderEmployeeCard(member)).join("")}
+          </main>
+        </body>
+      </html>
+    `;
+    // Returns the entire HTML page as a string
+    return pageHTML;
+}
