a15ac4407f0250ef4f9452c7cce9c3e56638699d
Reed Martin <reedhmartin@gmail.com>
2023-04-16 07:00:10 -0400

    added todos to creat team array, init function. Added todo to call init function.

--- src/script.js
+++ src/script.js
@@ -11,3 +11,14 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+// TODO: Create an array to store the team members
+let teamMembers = [];
+
+// TODO: Create a function to initialize the application
+function init() {
+  // TODO: Call the function to prompt for the manager's information
+  promptManager();
+}
+
+// TODO: Call the init() function to start the application
+init();
